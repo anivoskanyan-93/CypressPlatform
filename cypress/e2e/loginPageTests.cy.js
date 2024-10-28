c39d9e5430b2c671b2483e62627aaf39ba6ccf74
anivoskanyna-93 <ani.voskanyan@connectto.int>
2024-10-28 17:51:32 +0400

    classwork commit

---
+++ cypress/e2e/loginPageTests.cy.js
@@ -0,0 +1,21 @@
+import loginPage from "../PageObjects/loginPage";
+describe('Test', () => {
+
+    it('Test login with valid cred', () => {
+
+        //create instance for class
+        const loginPage = new LoginPage()
+
+        loginPage.open()
+        loginPage.login('Admin', 'admin123')
+        cy.contains('Dashboard')
+
+        //assert successful login
+        loginPage.assertSuccessfulLogin()
+
+        //assert failed login
+        //loginPage.assertErrorMessage()
+    });
+})
+
+
